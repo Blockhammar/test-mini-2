7b1b3793c4c8fdc9dc4df930ba46ed50c16e5478
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-02 13:24:18 +0100

    La till bild

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   //   .then(data => setData(data.results))
   // }, []);
 
-  const movieItems = data.map((x) => <div className="box" key={x.uid}> <img src='./Tatooine.jpeg' alt="text" /><p>{x.name}</p> </div>);
+  const movieItems = data.map((x) => <div className="box" key={x.uid}> <img src="https://media.sketchfab.com/models/6543e8e057ea4a85ba3586ce00e42b98/thumbnails/4e7d79f43e904b5e8c0e6d523e897ee8/4951d7fd286d4868ad692c352eb9b59e.jpeg" alt="text" /><p>{x.name}</p> </div>);
 
 function handelChange(event){
   setOption(event.target.value);
