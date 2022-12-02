be6474eed1d7836926c8f799252bbfdc3facf187
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-02 16:19:00 +0100

    Allt funkar

--- src/App.js
+++ src/App.js
@@ -45,14 +45,10 @@ function handelSubmit(event){
   return (
     <div className="App">
      
-      <div className="Header">
-        <img id="bild" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/200px-Star_Wars_Logo.svg.png" alt="" />
-        
         <form onSubmit={handelSubmit}>
         <input className ="tex"type="text" onChange={handelChange} />
         <input className="sub"type="submit" value="Submit" />
         </form>
-      </div>
       {thePerson}
     </div>
   );
