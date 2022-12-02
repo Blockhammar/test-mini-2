2a15d921145d9c4b545ab32079df5a7f887b7655
blockkhammar <blockhammar.programmering@gmail.com>
2022-12-02 13:11:07 +0100

    git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [option, setOption] = useState("people");
+
+  // useEffect(() => {
+  //   fetch("https://www.swapi.tech/api/people"
+  //   )
+  //   .then(response => response.json())
+  //   .then(data => setData(data.results))
+  // }, []);
+
+  const movieItems = data.map((x) => <div className="box" key={x.uid}> <img src='./Tatooine.jpeg' alt="text" /><p>{x.name}</p> </div>);
+
+function handelChange(event){
+  setOption(event.target.value);
+}
+
+function handelSubmit(event){
+  event.preventDefault();
+  fetch(`https://www.swapi.tech/api/${option}`)
+    .then(response => response.json())
+    .then(data => setData(data.results));
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handelSubmit}>
+       <input type="text" onChange={handelChange} />
+       <input type="submit" value="Submit" />
+      </form>
+
+       {movieItems}
     </div>
   );
 }
 
 export default App;
+
